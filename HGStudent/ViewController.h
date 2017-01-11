5a2c8850b95784ed2b2ad223dcfbbefd4e735ad8
李广金 <liguangjin@localhos>
2017-01-11 09:45:10 +0800

    Initial Commit

---
+++ HGStudent/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HGStudent
+//
+//  Created by DoronXC on 2017/1/11.
+//  Copyright © 2017年 HG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
